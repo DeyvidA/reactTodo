45d818c79d5d6613e221d261a87c0f2cf7f5eff3
DeyvidA <deyvida1001@gmail.com>
2022-01-25 22:04:19 -0600

    retoques de desinnio al calendario

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -109,51 +109,67 @@ const App = () => {
 
 						<div className="reminder reminderCalendar">
 							<table>
-								<tr>
-									<th>Mon</th>
-									<th>Tue</th>
-									<th>Wed</th>
-									<th>Thu</th>
-									<th>Fri</th>
-									<th>Sat</th>
-									<th>Sun</th>
-								</tr>
-								<tr>
-									<td>1</td>
-									<td>2</td>
-									<td>3</td>
-									<td>4</td>
-									<td>5</td>
-									<td>6</td>
-									<td>7</td>
-								</tr>
-								<tr>
-									<td>8</td>
-									<td>9</td>
-									<td>10</td>
-									<td>11</td>
-									<td>12</td>
-									<td>13</td>
-									<td>14</td>
-								</tr>
-								<tr>
-									<td>15</td>
-									<td>16</td>
-									<td>17</td>
-									<td>18</td>
-									<td>19</td>
-									<td>20</td>
-									<td>21</td>
-								</tr>
-								<tr>
-									<td>22</td>
-									<td>23</td>
-									<td>24</td>
-									<td>25</td>
-									<td className='day'>26</td>
-									<td>27</td>
-									<td>28</td>
-								</tr>
+								<thead>
+									<tr>
+										<th>Mon</th>
+										<th>Tue</th>
+										<th>Wed</th>
+										<th>Thu</th>
+										<th>Fri</th>
+										<th>Sat</th>
+										<th>Sun</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td className='lastMonth'>27</td>
+										<td className='lastMonth'>28</td>
+										<td className='lastMonth'>29</td>
+										<td className='lastMonth'>30</td>
+										<td className='lastMonth'>31</td>
+										<td>1</td>
+										<td>2</td>
+									</tr>
+									<tr>
+										<td>3</td>
+										<td>4</td>
+										<td>5</td>
+										<td>6</td>
+										<td>7</td>
+										<td>8</td>
+										<td>9</td>
+									</tr>
+									<tr>
+										<td>10</td>
+										<td>11</td>
+										<td>12</td>
+										<td>13</td>
+										<td>14</td>
+										<td>15</td>
+										<td>16</td>
+									</tr>
+									<tr>
+										<td>17</td>
+										<td>18</td>
+										<td>19</td>
+										<td>20</td>
+										<td>21</td>
+										<td>22</td>
+										<td>23</td>
+									</tr>
+									<tr>
+										<td>24</td>
+										<td>25</td>
+										<td className='day'>26</td>
+										<td>27</td>
+										<td>28</td>
+										<td>29</td>
+										<td>30</td>
+									</tr>
+									<tr>
+										<td>31</td>
+									</tr>
+								</tbody>
 							</table>
 						</div>
 					</div>
